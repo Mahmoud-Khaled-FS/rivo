251f0c22db8af6a566df91efd6c3baf409a3950f
Mahmoud-Khaled-FS <mahmoudkhaled1215@gmail.com>
2023-06-20 02:08:29 +0300

    clean repo

--- dist/src/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorReqHandler = (err, req, res, next) => {
-    if (err.code) {
-        res.status(err.code);
-    }
-    return res.json({ code: err.code || 400, status: 'failed', message: err.message });
-};
-exports.default = errorReqHandler;
